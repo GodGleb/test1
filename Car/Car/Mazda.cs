9a478caa2c257f73d4391d0b27e6f904a4569085
GodGleb <gachertilin@gmai.com>
2017-11-19 13:34:08 +0200

    первый коммит
    Signed-off-by: GodGleb <gachertilin@gmai.com>

---
+++ Car/Car/Mazda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    class Mazda
+    {
+        private string color; //цветмодель
+        private int power; // мощность двигателя
+        private int weight; //вес
+        public string model; //модель машины
+    }
+}
---
+++ Car/Car/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mazda Mazda1 = new Mazda(); //создание объекта Mazda1 класса Mazda
+            Mazda Mazda2 = new Mazda();
+
+            Mazda1.model = "model1";
+            Mazda2.model = "model2";
+
+            Console.WriteLine(Mazda1.model); // выводит на экран "model1"
+            Console.Write(Mazda2.model);
+            Console.ReadKey();
+        }
+    }
+}
